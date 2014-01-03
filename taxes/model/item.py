f501e59a35178d35cb380c441d67f51685b709c6
swift <swift@gmail.com>
2014-01-03 01:49:11 -0800

    make all test pass

--- taxes/model/item.py
+++ taxes/model/item.py
@@ -1,10 +1,18 @@
+import re
+
+PARSE_DESCRIPTION_PATTERN = "(\d+)\s(\w+(\s\w+)*)\sat\s(\d+\.\d+)"
+BOOK = ["book"]
+
 
 class Item:
     def __init__(self, description):
         self.description = description
-
-    def say_hi(self):
-        print 'hello world'
+        match = re.search(PARSE_DESCRIPTION_PATTERN, self.description)
+        self.count = match.group(1)
+        self.name = match.group(2)
+        self.price = match.group(4)
+        if self.name not in BOOK:
+            self.price = round(float(self.price) * 1.1, 2)
 
     def sale(self):
-        return "1 book: 12.49"
+        return self.count + " " + self.name + ": " + str(self.price)
--- test/unit/model/test_item.py
+++ test/unit/model/test_item.py
@@ -1,14 +1,17 @@
-
 import unittest
 from model.item import Item
 
 
 class MyTestCase(unittest.TestCase):
-
     def test_1_book_at_12_dot_49(self):
         self.item = Item("1 book at 12.49")
         result = self.item.sale()
         self.assertEqual(result, "1 book: 12.49")
+
+    def test_1_music_CD_at_14_dot_99(self):
+        self.item = Item("1 music CD at 14.99")
+        result = self.item.sale()
+        self.assertEqual(result, "1 music CD: 16.49")
 
 
 if __name__ == '__main__':

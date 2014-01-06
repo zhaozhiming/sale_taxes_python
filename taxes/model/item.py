2cc4fb02e0d748959ae305d2bc5884404e79c84a
swift <swift@gmail.com>
2014-01-05 19:15:29 -0800

    fix item fail test case

--- taxes/model/item.py
+++ taxes/model/item.py
@@ -1,5 +1,8 @@
 import re
 
+COUNT_INDEX = 1
+NAME_INDEX = 2
+PRICE_INDEX = 4
 PARSE_DESCRIPTION_PATTERN = "(\d+)\s(\w+(\s\w+)*)\sat\s(\d+\.\d+)"
 BOOK = ["book"]
 FOOD = ["chocolate bar"]
@@ -7,15 +10,15 @@ FOOD = ["chocolate bar"]
 
 class Item:
     def __init__(self, description):
-        self.description = description
-        match = re.search(PARSE_DESCRIPTION_PATTERN, self.description)
-        self.count = match.group(1)
-        self.name = match.group(2)
-        self.price = match.group(4)
+        match = re.search(PARSE_DESCRIPTION_PATTERN, description)
+        self.count = match.group(COUNT_INDEX)
+        self.name = match.group(NAME_INDEX)
+        self.price = price = round(float(match.group(PRICE_INDEX)), 2)
+
         self.tax = 0
         if self.name not in BOOK + FOOD:
-            self.price = round(float(match.group(4)) * 1.1, 2)
-            self.tax = round(float(match.group(4)) * 0.1, 2)
+            self.price = round(price * 1.1, 2)
+            self.tax = round(price * 0.1, 2)
 
 
     def sale(self):

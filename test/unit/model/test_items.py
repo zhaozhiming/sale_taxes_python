2363e50533c211c05f5aed58e3c4d2967efb07b2
swift <swift@gmail.com>
2014-01-05 19:24:00 -0800

    refactor duplicate code

--- test/unit/model/test_items.py
+++ test/unit/model/test_items.py
@@ -4,19 +4,17 @@ from model.items import Items
 
 
 class ItemsTestCase(unittest.TestCase):
-    def test_scenario_1_tax(self):
+    def setUp(self):
         item1 = Item("1 book at 12.49")
         item2 = Item("1 music CD at 14.99")
         item3 = Item("1 chocolate bar at 0.85")
-        self.items = Items([item1, item2, item3])
-        self.assertEqual(self.items.tax(), "Sales Taxes: 1.50")
+        self.scenario1_items = Items([item1, item2, item3])
+
+    def test_scenario_1_tax(self):
+        self.assertEqual(self.scenario1_items.tax(), "Sales Taxes: 1.50")
 
     def test_scenario_1_total(self):
-        item1 = Item("1 book at 12.49")
-        item2 = Item("1 music CD at 14.99")
-        item3 = Item("1 chocolate bar at 0.85")
-        self.items = Items([item1, item2, item3])
-        self.assertEqual(self.items.total(), "Total: 29.83")
+        self.assertEqual(self.scenario1_items.total(), "Total: 29.83")
 
 
 if __name__ == '__main__':

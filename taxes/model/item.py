5364529b6fe86ee15e5325b2055f3a25dde3c799
swift <swift@gmail.com>
2014-01-05 19:32:16 -0800

    refactor

--- taxes/model/item.py
+++ taxes/model/item.py
@@ -1,22 +1,24 @@
 import re
 
-COUNT_INDEX = 1
-NAME_INDEX = 2
-PRICE_INDEX = 4
-PARSE_DESCRIPTION_PATTERN = "(\d+)\s(\w+(\s\w+)*)\sat\s(\d+\.\d+)"
-BOOK = ["book"]
-FOOD = ["chocolate bar"]
+
+class Constant(object):
+    COUNT_INDEX = 1
+    NAME_INDEX = 2
+    PRICE_INDEX = 4
+    PARSE_DESCRIPTION_PATTERN = "(\d+)\s(\w+(\s\w+)*)\sat\s(\d+\.\d+)"
+    BOOK = ["book"]
+    FOOD = ["chocolate bar"]
 
 
 class Item:
     def __init__(self, description):
-        match = re.search(PARSE_DESCRIPTION_PATTERN, description)
-        self.count = match.group(COUNT_INDEX)
-        self.name = match.group(NAME_INDEX)
-        self.price = price = round(float(match.group(PRICE_INDEX)), 2)
+        match = re.search(Constant.PARSE_DESCRIPTION_PATTERN, description)
+        self.count = match.group(Constant.COUNT_INDEX)
+        self.name = match.group(Constant.NAME_INDEX)
+        self.price = price = round(float(match.group(Constant.PRICE_INDEX)), 2)
 
         self.tax = 0
-        if self.name not in BOOK + FOOD:
+        if self.name not in Constant.BOOK + Constant.FOOD:
             self.price = round(price * 1.1, 2)
             self.tax = round(price * 0.1, 2)
 

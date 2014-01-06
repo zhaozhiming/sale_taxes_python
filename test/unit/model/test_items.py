aa92e7bb2035e795b135a953875bde7949f1384a
swift <swift@gmail.com>
2014-01-06 01:04:51 -0800

    add scenario 3 items test case

--- test/unit/model/test_items.py
+++ test/unit/model/test_items.py
@@ -14,6 +14,12 @@ class ItemsTestCase(unittest.TestCase):
         item5 = Item("1 imported bottle of perfume at 47.50")
         self.scenario2_items = Items([item4, item5])
 
+        item6 = Item("1 imported bottle of perfume at 27.99")
+        item7 = Item("1 bottle of perfume at 18.99")
+        item8 = Item("1 packet of headache pills at 9.75")
+        item9 = Item("1 box of imported chocolates at 11.25")
+        self.scenario3_items = Items([item6, item7, item8, item9])
+
     def test_scenario_1_tax(self):
         self.assertEqual(self.scenario1_items.tax(), "Sales Taxes: 1.50")
 
@@ -25,6 +31,12 @@ class ItemsTestCase(unittest.TestCase):
 
     def test_scenario_2_total(self):
         self.assertEqual(self.scenario2_items.total(), "Total: 65.15")
+
+    def test_scenario_3_tax(self):
+        self.assertEqual(self.scenario3_items.tax(), "Sales Taxes: 6.70")
+
+    def test_scenario_3_total(self):
+        self.assertEqual(self.scenario3_items.total(), "Total: 74.68")
 
 
 if __name__ == '__main__':

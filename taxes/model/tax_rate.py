618f9caf53e405798deac279bfaf20fbab3fb783
swift <swift@gmail.com>
2014-01-06 02:25:53 -0800

    refactor code

--- taxes/model/tax_rate.py
+++ taxes/model/tax_rate.py
@@ -1,15 +1,24 @@
+from abc import abstractmethod
 from model.constant import Constant
 
 
-class TaxRate:
+class TaxRate():
     def __init__(self):
         pass
 
     def tax_rate(self, name):
         tax_rate = 0
-        item_name = name.replace("%s " % Constant.IMPORTED_TEXT_IDENTIFY, "")
-        if item_name not in Constant.BOOK + Constant.FOOD + Constant.MEDICAL:
+        if self.item_not_in_exemptions_list(name):
             tax_rate += Constant.BASE_TAXES
-        if Constant.IMPORTED_TEXT_IDENTIFY in name:
+        if self.item_is_imported(name):
             tax_rate += Constant.IMPORTED_TAXES
         return tax_rate
+
+    @abstractmethod
+    def item_not_in_exemptions_list(self, name):
+        item_name = name.replace("%s " % Constant.IMPORTED_TEXT_IDENTIFY, "")
+        return item_name not in Constant.BOOK + Constant.FOOD + Constant.MEDICAL
+
+    @abstractmethod
+    def item_is_imported(self, name):
+        return Constant.IMPORTED_TEXT_IDENTIFY in name
